bff7dca79357fed6161a42504ed7b09831d898ea
MaelleCoquelet <maelle.coquelet@edu.univ-fcomte.fr>
2024-04-29 16:59:41 +0200

    ajout de la boucle de données

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -53,6 +53,7 @@ const maisonsListe: MaisonRecord[] = [
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
-  <MaisonCard v-bind="maisonsListe[0]"></MaisonCard>
-  <MaisonCard v-bind="maisonsListe[1]"></MaisonCard>
+  <!-- <MaisonCard v-bind="maisonsListe[0]"></MaisonCard>
+  <MaisonCard v-bind="maisonsListe[1]"></MaisonCard> -->
+  <MaisonCard v-for="maison in maisonsListe" v-bind="maison" :key="MaisonCard.nomMaison"></MaisonCard>
 </template>
